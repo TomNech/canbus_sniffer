6adf713cf609fdee3b251cc9238f01855ad9b790
„TomNech“ <Tom-Nech@seznam.cz>
2023-06-13 18:27:28 +0200

    dict sort key and value

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import sqlite3
+# import sqlite3
 import argparse
 import sys
 from loader import Loader
 from parser import Parser
 from counter import MessageCounter
-from database import SQLite
-
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        prog='Moje krásná aplikace',
-        description='Umím parsovat CAN zprávy',
-        epilog='Celé jsem to napsal sám')
+        prog='CAN_MONITOR',
+        description='MONITOR_CAN_MESSAGE',
+        epilog='')
 
     parser.add_argument('-f', '--filename', help="Zadej název souboru")
     parser.add_argument('-db', '--database', help="Zadej název souboru")
@@ -32,9 +30,6 @@ if __name__ == '__main__':
     def all_msg():
 
         num = 0
-        # for a in range(13):
-        #     parser.parse_line(loader.get_data(a))
-        #     parser.data
 
         print(f"Zjištěné CAN_BUS zprávy: {loader}")
 
@@ -56,7 +51,7 @@ if __name__ == '__main__':
             if key.startswith(can_id):
                 filter_msg.update({key: value})
 
-        for key, value in filter_msg.items():
+        for key, value in (sorted(filter_msg.items(), key=lambda item: item[1])):
             num += 1
             print(f"{num}.\t CAN zpráva: {key} \t POČET: {value}")
         print()
@@ -69,13 +64,11 @@ if __name__ == '__main__':
         num = 0
         can_num = int(input("Zadejte počet výskytů CAN zprávy: "))
 
-        # print(dictionary.get_counter_list.items())
-
         for key, value in dictionary.get_counter_list.items():
             if value == can_num:
                 filter_msg.update({key: value})
 
-        for key, value in filter_msg.items():
+        for key, value in sorted(filter_msg.items()):
             num += 1
             print(f"{num}.\t CAN zpráva: {key} \t POČET: {value}")
         print()
@@ -103,34 +96,33 @@ if __name__ == '__main__':
 
 
     while True:
-        try:
-            print(loader)
-            print("1 - Zobrazit všechny nalezené zprávy")
-            print("2 - Zobrazit seznam podle vybraného ID CAN zprávy")
-            print("3 - Zobrazit seznam podle počtu výskytů CAN zprávy")
-            print("4 - Zobrazit seznam podle rozsahu výskytů CAN zprávy")
-            print("5 - Ukončit program")
-            print("--------------------------------------------------")
 
-            choice = int(input("Zadejte volbu: "))
+        print(loader)
+        print("1 - Zobrazit všechny nalezené zprávy")
+        print("2 - Zobrazit seznam podle vybraného ID CAN zprávy")
+        print("3 - Zobrazit seznam podle počtu výskytů CAN zprávy")
+        print("4 - Zobrazit seznam podle rozsahu výskytů CAN zprávy")
+        print("5 - Ukončit program")
+        print("--------------------------------------------------")
 
-            if choice == 1:
-                all_msg()
+        choice = int(input("Zadejte volbu: "))
 
-            elif choice == 2:
-                filter_key_msg()
+        if choice == 1:
+            all_msg()
 
-            elif choice == 3:
-                filter_value_msg()
+        elif choice == 2:
+            filter_key_msg()
 
-            elif choice == 4:
-                filter_range_value()
+        elif choice == 3:
+            filter_value_msg()
 
-            elif choice == 5:
-                end()
+        elif choice == 4:
+            filter_range_value()
 
-            else:
-                print("Volba neexistuje\n")
+        elif choice == 5:
+            end()
 
-        except:
-            print("Chyba! Zadejte volbu.")
+        else:
+            print("Volba neexistuje\n")
+
+
